f357ed526a9e1d09f5776b2306a6339abac7aa4c
Charlie <77836344+TheKingChu@users.noreply.github.com>
2024-01-26 23:11:33 +0100

    audience spawner

--- GGJ2024Bakom/Assets/__Scripts/Audience/AudienceSpawner.cs
+++ GGJ2024Bakom/Assets/__Scripts/Audience/AudienceSpawner.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class AudienceSpawner : MonoBehaviour
 {
+    public GameObject audiencePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpawnAudience();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SpawnAudience()
     {
         
     }
